8c78e7de1e7e8e76d7a6b96a3606dfcd84358107
sapho-sys <saphonkunzi@gmail.com>
2022-07-05 12:32:11 +0200

    done!

--- greet_xola.js
+++ greet_xola.js
@@ -1,9 +1,17 @@
 // const chalk = require('chalk');
 import chalk from 'chalk';
+import figlet from "figlet";
 
 //import the greet module that is in the current folder
-// const greet = require('./greet');
 import greet from './greet.js'
+figlet(greet("Sapho"), function(err, data) {
+    if (err) {
+        console.log(chalk.red('Something went wrong...'));
+        console.dir(chalk.blue(err));
+        return;
+    }
+    console.log(data)
+});
 
 const styledMessage = chalk.bgGreen.black(greet('Xola'));
 console.log(styledMessage)
